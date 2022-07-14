69fe6a5b4eeb3646011d669937f6530503d71211
traktorbless <90085453+traktorbless@users.noreply.github.com>
2022-07-14 12:29:07 +0300

    create project

---
+++ ServicesOfVk/ServicesOfVkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServicesOfVkApp.swift
+//  ServicesOfVk
+//
+//  Created by Антон Таранов on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ServicesOfVkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
